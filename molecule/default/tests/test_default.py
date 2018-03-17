1cc05dd22c02f87328a0f1384847418f1b3cec51
Dave Selders <david.selders@gmail.com>
2018-03-17 12:44:40 -0700

    Initial Quagga work enable zebra and osfpd
    defaults/main.yml:
    	ospfd is disabled by default
    
    tasks/main.yml:
    	Install quagga package, enable zebra daemon.  If
    	quagga.ospfd_enabled is True then enable ospfd
    
    molecule/default/tests/test_default.py:
    	- quagga package is installed
    	- zebra is enabled
    	- ospfd is enabled
    
    molecule/default/molecule.yml:
    	Add ansible group to handle ospf, setting the
    	quagga_ospfd_enabled variable to True

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_quagga_is_installed(host):
+    quagga = host.package("quagga")
+    daemons = host.file("/etc/quagga/daemons")
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert quagga.is_installed
+    assert daemons.is_file
+
+
+def test_zebra_is_enabled(host):
+    daemons = host.file("/etc/quagga/daemons")
+
+    assert daemons.is_file
+    assert daemons.contains("^zebra=yes$")
+
+
+def test_ospfd_is_enabled(host):
+    daemons = host.file("/etc/quagga/daemons")
+
+    assert daemons.is_file
+    assert daemons.contains("^ospfd=yes$")
