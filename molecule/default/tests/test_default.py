d62c84eda660e9dbcddf6d5ce766bb6c96b9ba5c
Dave Selders <david.selders@gmail.com>
2018-03-17 19:03:10 -0700

    Added missing tests for OSPF settings
    Tests were missing for :
    
    - OSPF router-id
    - OSPF network advertisement statement
    - OSPF passive-interface
    - OSPF refernce bandwidth
    
    Extended zebra template to handle the setting of interface bandwidth to
    aid in the OSPF cost calculation.  Corrected mis-named variables in the
    ospfd template, preventing correct placement of the passive-interface
    statement.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -45,3 +45,27 @@ def test_ospfd_is_listening(host):
     ospfd_socket = host.socket("tcp://127.0.0.1:2604")
 
     assert ospfd_socket.is_listening
+
+
+def test_ospfd_router_id_is_set(host):
+    router_id = host.run("vtysh -c 'sh run' | grep 'ospf router-id'")
+
+    assert router_id.stdout == " ospf router-id 1.1.1.1"
+
+
+def test_ospfd_network_is_advertised(host):
+    network = host.run("vtysh -c 'sh run' | grep 'network 192.168.29.0'")
+
+    assert network.stdout == " network 192.168.29.0/24 area 0.0.0.0"
+
+
+def test_ospfd_lo0_is_passive(host):
+    passive_intf = host.run("vtysh -c 'sh run' | grep 'passive-interface lo0'")
+
+    assert passive_intf.stdout == " passive-interface lo0"
+
+
+def test_ospfd_eth1_bw_is_set(host):
+    eth1_bw = host.run("vtysh -c 'sh int eth1' | grep bandwidth")
+
+    assert eth1_bw.stdout == "  bandwidth 1000000 kbps"
